2c994ea28c8706ce4e1074c90a897cc2c6b04eaf
gdufeZLYL <18027894043@163.com>
2018-05-02 22:28:23 +0800

    *

---
+++ Java/JUC/code/CyclicBarrierTest.java
@@ -0,0 +1,37 @@
+package com.qexz;
+
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author zzqnxx@foxmail.com
+ * @date 2018/4/29 8:47
+ */
+public class CyclicBarrierTest {
+
+    private static CyclicBarrier cyclicBarrier;
+
+    static class CyclicBarrierThread extends Thread{
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + "到了");
+            //等待
+            try {
+                cyclicBarrier.await();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        cyclicBarrier = new CyclicBarrier(5, new Runnable() {
+            public void run() {
+                System.out.println("人到齐了，开会吧....");
+            }
+        });
+
+        for(int i = 0 ; i < 5 ; i++){
+            new CyclicBarrierThread().start();
+        }
+    }
+
+}
